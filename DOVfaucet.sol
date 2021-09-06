e0e02904c14c14533e303bb0a281b0256476dc72
Matt Smithies <flyinggazelle@hotmail.co.uk>
2021-09-06 21:41:22 +0100

    Initial commit

---
+++ DOVfaucet.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "./@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./@openzeppelin/contracts/security/Pausable.sol";
+import "./@openzeppelin/contracts/access/Ownable.sol";
+
+contract DOVfaucet is ERC20, Pausable, Ownable {
+    constructor() ERC20("DOVfaucet", "tDOV") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# test-dov-faucet
-DOV Faucet for MVP Marketplace 
+# DOV Faucet for MVP and Sandbox APIs 
+
+This is a faucet to use DOV on our MVPs and sandbox APIs.
+
+DM @happytitan on telegram or send an email to support@dovu.io with the following information:
+
+- The network that you are using
+= The Address of your wallet
+  
+You'll be airdropped some tDOV (Test DOV tokens) to use for the MVP Marketplace or our testnet API sandboxes.
+
+## TODO
+
+We'll provide a small webapp for people to mint tokens without the manual process.
